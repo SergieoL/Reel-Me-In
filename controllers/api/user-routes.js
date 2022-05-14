c01537f3cea5399315822bb0c9444a83bc277089
Jaime Rivas <j.rivas4818@gmail.com>
2022-05-14 12:44:26 -0700

    updated post.js models

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,6 +41,7 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
+    
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
@@ -48,9 +49,25 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.post('/login', (req, res) => {
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        res.json({ user: dbUserData });
+
+    
+      });  
+    });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
