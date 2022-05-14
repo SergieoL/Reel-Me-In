f7cd5a0d995f1c24ea68555bba2c6a0d34b2ecd8
Sergieo <sergieol23@gmail.com>
2022-05-13 23:11:13 -0500

    Can now render pages when sever is started

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,16 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/create-account', (req, res) => {
+  res.render('create-account');
+});
+
+router.get('/single-post', (req, res) => {
+  res.render('single-post');
+})
+
 module.exports = router;
