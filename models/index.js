ec3339a2e7e7e9ff62137287e67608f4ff120644
Sergieo <sergieol23@gmail.com>
2022-05-16 00:40:17 -0500

    Comment model created

--- models/index.js
+++ models/index.js
@@ -1,1 +1,38 @@
-// importd Models and 
+// importd Models and exports as objects with it as a property
+const User = require('./User');
+const Review = require('./Review');
+const Comment = require('./Comment');
+
+// create associations
+
+// a user can have many reviews
+User.hasMany(Review, {
+    foreignKey: 'user_id'
+});
+
+// a review can only have one user
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// a comment can only have one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// a comment can only belong to one review
+Comment.belongsTo(Review, {
+    foreignKey: 'review_id'
+})
+
+// a user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+// a review can have many comments
+Review.hasMany(Comment, {
+    foreignKey: 'review_id'
+})
+
+module.exports = { User, Review, Comment };
