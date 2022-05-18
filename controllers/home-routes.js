9b1369e29d2e29643387783dd6011b08433f9d32
Sergieo <sergieol23@gmail.com>
2022-05-17 23:22:47 -0500

    fetches all reviews and passes it to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,38 @@
+const { Review, Comment, User } = require('../models');
+const sequelize = require('../config/connection');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.render('homepage');
-});
-
-// router.get('/', (req, res) => {
-//   res.render('homepage', {
-//     posts,
-//     loggedIn: req.session.loggedIn
-//   });
-// });
-
+// get all reviews for homepage
+router.get('/', (req, res ) => {
+  Review.findAll({
+      order: [['created_at', 'DESC']],
+      // include user that created the review
+      include: [
+          {
+              model: User,
+              attributes: ['username']
+          },
+          // includes all comments and user that posted them
+          {
+              model: Comment,
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+          }
+      ]
+  })
+  .then(dbReviewData => {
+    const reviews = dbReviewData.map(review => review.get({ plain: true }));
+    res.render('homepage', {
+      reviews
+    });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  })
+})
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
@@ -20,7 +42,6 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
-
 
 router.get('/create-account', (req, res) => {
   res.render('create-account');
