c2c552481928729a5ab7288e5dd70ba209017ec1
Sergieo <sergieol23@gmail.com>
2022-05-19 00:09:51 -0500

    movie data redners on single review page

--- public/javascript/movie-data.js
+++ public/javascript/movie-data.js
@@ -1,31 +1,23 @@
 //imdb api key
 const imdbKey = 'k_wgnnz7cy';
+const movieTitle = document.getElementById('movieTitle').innerHTML;
 
 // fetch imdb for movie data
 const searchMovie = function (movieTitle) {
-    apiUrl = "https://imdb-api.com/en/API/Search/" + imdbKey + "/" + 'inception';
+    apiUrl = "https://imdb-api.com/en/API/Search/" + imdbKey + "/" + movieTitle;
 
 
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            return data.results[0];
+            movieData = data.results[0];
+            console.log(movieData);
+
+            document.getElementById("moviePoster").innerHTML = `<img src="${movieData.image}" width=300px>`;
+            document.getElementById("movieYear").innerHTML = movieData.description;
+            
+
         })
     })
 }
 
-
-
-const addMovieData = function() {
-
-    // get move title for search from 'single
-    var movieTitle = document.getElementById('movieTitle').value.trim();
-
-    movieData = searchMovie(movieTitle);
-
-    // Adding html code to review page
-    document.getElementById("movie-poster").innerHTML = `<img src="${movieData.image}" width=300px>`
-    document.getElementById("year").innerHTML = movieData.description;
-
-}
-
-addMovieData();
+searchMovie(movieTitle);
